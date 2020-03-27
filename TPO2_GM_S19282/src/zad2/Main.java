41ef38a036b827eee8eed74dc01958c83e7d0b2c
Mateusz Godlewski <s19282@pjwstk.edu.pl>
2020-03-27 22:46:28 +0100

    Klasa Service gotowa

--- TPO2_GM_S19282/src/zad2/Main.java
+++ TPO2_GM_S19282/src/zad2/Main.java
@@ -7,21 +7,19 @@
 package zad2;
 
 
-import org.xml.sax.SAXException;
-
-import javax.xml.parsers.ParserConfigurationException;
+import org.json.JSONException;
 import java.io.IOException;
 
 public class Main
 {
-  public static void main(String[] args) throws IOException, ParserConfigurationException, SAXException {
-    Service s = new Service("Poland");
+  public static void main(String[] args) throws IOException, JSONException {
+    Service s = new Service("United States");
     String weatherJson = s.getWeather("Warsaw");
     System.out.println(weatherJson);
-    Double rate1 = s.getRateFor("USD");
+    Double rate1 = s.getRateFor("JPY");
     System.out.println(rate1);
     Double rate2 = s.getNBPRate();
-    //System.out.println(rate2);
+    System.out.println(rate2);
     // część uruchamiająca GUI
   }
 }
--- TPO2_GM_S19282/src/zad2/Service.java
+++ TPO2_GM_S19282/src/zad2/Service.java
@@ -6,22 +6,13 @@
 
 package zad2;
 
-import jdk.internal.org.xml.sax.XMLReader;
-import org.json.JSONObject;
 import org.json.JSONArray;
-import org.w3c.dom.Document;
-import org.xml.sax.SAXException;
-import org.xml.sax.helpers.XMLReaderFactory;
+import org.json.JSONException;
+import org.json.JSONObject;
 
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-import java.beans.XMLDecoder;
 import java.io.*;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
-import java.nio.channels.FileChannel;
 import java.util.Currency;
 import java.util.HashMap;
 import java.util.Locale;
@@ -30,31 +21,34 @@ public class Service implements Serializable
 {
     private static final String apiKey = "59045bb3edb0ac8183aee91bb2c18ee9";
     Locale locale;
-
-    HashMap<String,String> countryCurrency = new HashMap<>();
+    public static HashMap<String,String> countryCode = new HashMap<>();
+    public static HashMap<String,String> codeCurrency = new HashMap<>();
 
     public Service(String country)
     {
         locale = new Locale("",country);
         for(String loc: Locale.getISOCountries())
         {
-            try
-            {
-                countryCurrency.put(new Locale("",loc).getDisplayCountry().toUpperCase(),Currency.getInstance(new Locale("",loc)).getCurrencyCode());
-            }
-            catch (NullPointerException ignored)
-            {}
+            Locale tmp = new Locale("",loc);
+            try { codeCurrency.put(loc,Currency.getInstance(tmp).getCurrencyCode());}
+            catch (NullPointerException ignored){}
+            countryCode.put(tmp.getDisplayCountry().toUpperCase(),loc);
         }
     }
 
+
+    public static String getCurrency(Locale country)
+    {
+        return codeCurrency.get(countryCode.get(country.getDisplayCountry()));
+    }
+
+
     public String getURLConnection(String link) throws IOException
     {
-        //int responseCode;
-        StringBuilder response = null;
+        StringBuilder response;
         String lineToRead;
         URL url = new URL(link);
         HttpURLConnection httpConnection = (HttpURLConnection) url.openConnection();
-        //responseCode = httpConnection.getResponseCode();
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(httpConnection.getInputStream()));
         response = new StringBuilder();
         while ((lineToRead = bufferedReader.readLine()) != null)
@@ -63,31 +57,51 @@ public class Service implements Serializable
         return response.toString();
     }
 
-    public String getWeather(String city) throws IOException
-    {
+
+    public String getWeather(String city) throws IOException, JSONException {
         JSONObject object = new JSONObject(getURLConnection("http://api.openweathermap.org/data/2.5/weather?q="+city+"&appid="+apiKey));
         return object.toString();
     }
 
-    public double getRateFor(String currency) throws IOException
-    {
-        if(countryCurrency.get(locale.getDisplayCountry()).equals(currency))
+
+    public double getRateFor(String currency) throws IOException, JSONException {
+        if(currency.equalsIgnoreCase(getCurrency(locale)))
             return 1D;
         else
         {
-            JSONObject object = new JSONObject(getURLConnection("https://api.exchangeratesapi.io/latest?symbols=PLN,"+currency));
+            JSONObject object = new JSONObject(getURLConnection("https://api.exchangeratesapi.io/latest?base="+getCurrency(locale)+"&symbols="+currency));
             JSONObject rates = new JSONObject(object.get("rates").toString());
-
-            return Double.parseDouble(rates.get(countryCurrency.get(locale.getDisplayCountry())).toString());
+            return rates.getDouble(currency);
         }
     }
 
-    public double getNBPRate() throws IOException {
-       XMLDecoder test = new XMLDecoder(getURLConnection("http://www.nbp.pl/kursy/xml/a058z200324.xml").);
-       Tabela_kursow tk = (Tabela_kursow)test.readObject();
-        System.out.println();
-        return 1d;
+
+    public double getNBPRate() throws IOException, JSONException {
+       if(locale.getDisplayCountry().equalsIgnoreCase("POLAND"))
+       {
+           return 1D;
+       }
+       else
+       {
+           JSONObject xml;
+           try
+           {
+               xml = new JSONObject(getURLConnection("http://api.nbp.pl/api/exchangerates/rates/a/"+getCurrency(locale)+"/"));
+           }
+           catch (FileNotFoundException e)
+           {
+               try
+               {
+                   xml = new JSONObject(getURLConnection("http://api.nbp.pl/api/exchangerates/rates/b/"+getCurrency(locale)+"/"));
+               }
+               catch (FileNotFoundException e1)
+               {
+                   System.out.println("Nie znaleziono waluty");
+                   return -1D;
+               }
+           }
+           JSONArray value = xml.getJSONArray("rates");
+           return value.getJSONObject(0).getDouble("mid");
+       }
     }
-
-
 }  
