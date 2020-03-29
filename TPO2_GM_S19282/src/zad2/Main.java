2ebc0274557569563127b67dcf86b855114943e0
Mateusz Godlewski <s19282@pjwstk.edu.pl>
2020-03-29 21:58:43 +0200

    Usunięte testowe kolory w aplikacji, poprawione formatowanie kodu

--- TPO2_GM_S19282/src/zad2/Main.java
+++ TPO2_GM_S19282/src/zad2/Main.java
@@ -8,7 +8,6 @@ package zad2;
 
 
 import javafx.application.Application;
-import javafx.application.Platform;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
@@ -24,7 +23,6 @@ import javafx.stage.Stage;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-
 import java.io.File;
 import java.text.DecimalFormat;
 
@@ -38,97 +36,81 @@ public class Main extends Application
   @Override
   public void start(Stage primaryStage)
   {
-      //naprawić ponowne ok
       primaryStage.setTitle("Webclients");
 
       VBox mainBox = new VBox();
-      mainBox.setAlignment(Pos.BASELINE_CENTER);
       HBox top = new HBox();
       VBox inputData = new VBox();
       VBox rates = new VBox();
       HBox weather = new HBox();
-      Label rate1 = new Label();
-      Label rate2 = new Label();
-      rate1.setAlignment(Pos.CENTER_LEFT);
-      rate2.setAlignment(Pos.CENTER_LEFT);
-      rate1.setStyle("-fx-background-color: #d4ff79;");
-      rate2.setStyle("-fx-background-color: #5dffd6;");
       WebView webView = new WebView();
       WebEngine webEngine = webView.getEngine();
-      webView.setMaxWidth(1000);
-      webView.setStyle("-fx-alignment: center");
-      mainBox.setStyle("-fx-background-color: #7574ff;");
-      inputData.setStyle("-fx-background-color: #5cff6f;");
-      inputData.setPadding(new Insets(10));
-      weather.setAlignment(Pos.CENTER_RIGHT);
-      weather.setStyle("-fx-font-size: 15;" +
-              "-fx-background-color: #7d05ff;");
-      weather.setPadding(new Insets(0,0,0,380));
-      rates.setAlignment(Pos.CENTER_LEFT);
-      rates.setStyle("-fx-font-size: 15;" +
-              "-fx-background-color: #4553ff;");
-      rates.setPadding(new Insets(0,30,0,30));
-      //rates.setStyle("-fx-background-color: #141dff;");
-     // weather.setStyle("-fx-background-color: #7d05ff;");
-
-      //inputData.setMaxSize(150,240);
-      top.setStyle("-fx-background-color: #ffb64e;");
-
-
       TextField country = new TextField("Enter country");
       TextField city = new TextField("Enter city");
       TextField currency = new TextField("Enter currency");
       Button button = new Button("OK");
+      ImageView notFound = new ImageView(new Image(new File("img/notFound.png").toURI().toString()));
+
+      mainBox.setStyle("-fx-background-color: rgba(76,108,255,0.69);");
+      mainBox.setAlignment(Pos.BASELINE_CENTER);
+      top.setPadding(new Insets(0,0,20,0));
+      inputData.setPadding(new Insets(10));
+      weather.setStyle("-fx-font-size: 15;");
+      weather.setPadding(new Insets(0,0,0,380));
+      rates.setAlignment(Pos.CENTER_LEFT);
+      rates.setStyle("-fx-font-size: 15;");
+      rates.setPadding(new Insets(0,30,0,30));
+      webView.setMaxWidth(1000);
       country.setStyle("-fx-background-insets: 5px;");
       city.setStyle("-fx-background-insets: 5px;");
       currency.setStyle("-fx-background-insets: 5px;");
       button.setStyle("-fx-background-insets: 5px;");
-      mainBox.getChildren().addAll(top);
-      ImageView notFound = new ImageView(new Image(new File("img/notFound.png").toURI().toString()));
-
-
       button.setOnAction(event ->
       {
-          weather.getChildren().clear();
-          mainBox.getChildren().removeAll(notFound,webView);
           Service service = new Service(country.getText());
+          Label rate1 = new Label();
+          Label rate2 = new Label();
+
+          weather.getChildren().clear();
+          rates.getChildren().clear();
+          mainBox.getChildren().removeAll(notFound,webView);
+          rate1.setAlignment(Pos.CENTER_LEFT);
+          rate2.setAlignment(Pos.CENTER_LEFT);
+          webEngine.load("https://en.wikipedia.org/wiki/"+city.getText());
 
           if(service.getRateFor(currency.getText())==-1)
               rate1.setText("Oops...there was a problem, restart app or try again");
           else
               rate1.setText("1"+service.getCurrency()+" = "+service.getRateFor(currency.getText())+currency.getText().toUpperCase());
+
           if(service.getNBPRate()==-1)
               rate2.setText("Oops...there was a problem, restart app or try again");
           else
               rate2.setText("1PLN = "+service.getNBPRate()+service.getCurrency());
 
-          webEngine.load("https://en.wikipedia.org/wiki/"+city.getText());
-
+          rates.getChildren().addAll(rate1,rate2);
 
           try
           {
               VBox left = new VBox();
-              Label labelL = new Label();
               VBox iconAndDescription = new VBox();
-              iconAndDescription.setStyle("-fx-background-color: #ff0f23;");
-              left.setStyle("-fx-background-color: #ff9a06;");
-              left.setAlignment(Pos.CENTER);
-              Label labelC = new Label();
               VBox right = new VBox();
+              Label labelL = new Label();
+              Label labelC = new Label();
               Label laberR = new Label();
-              right.setAlignment(Pos.CENTER);
-              right.setStyle("-fx-background-color: #ff1ec1;");
               StringBuilder sb = new StringBuilder();
               JSONObject json = new JSONObject(service.getWeather(city.getText()));
               JSONArray array =  json.getJSONArray("weather");
               String iconID = array.getJSONObject(0).getString("icon");
               Image icon = new Image("http://openweathermap.org/img/wn/"+iconID+"@2x.png");
-              labelC.setText(array.getJSONObject(0).getString("description"));
-              iconAndDescription.setAlignment(Pos.CENTER);
               BorderPane bp = new BorderPane();
-              bp.setCenter(new ImageView(icon));
-              iconAndDescription.getChildren().addAll(bp,labelC);
               JSONObject important = (JSONObject) json.get("main");
+
+              left.setAlignment(Pos.CENTER);
+              right.setAlignment(Pos.CENTER);
+              iconAndDescription.setAlignment(Pos.CENTER);
+              labelC.setText(array.getJSONObject(0).getString("description"));
+              bp.setCenter(new ImageView(icon));
               sb.append("Temperature: ").append(new DecimalFormat("##.0").format(important.getDouble("temp")-273.25))
                       .append("\nTemperature min: ").append(new DecimalFormat("##.0").format(important.getDouble("temp_min")-273.25))
                       .append("\nTemperature max: ").append(new DecimalFormat("##.0").format(important.getDouble("temp_max")-273.25));
@@ -138,30 +120,24 @@ public class Main extends Application
                       .append("\nHumidity: ").append(important.getInt("humidity"))
                       .append("\nPressure: ").append(important.getInt("pressure"));
               laberR.setText(sb.toString());
-              left.getChildren().addAll(labelL);
-              right.getChildren().addAll(laberR);
+              left.getChildren().add(labelL);
+              iconAndDescription.getChildren().addAll(bp,labelC);
+              right.getChildren().add(laberR);
               weather.getChildren().addAll(left,iconAndDescription,right);
-
-              //mainBox.getChildren().remove(loading);
               mainBox.getChildren().add(webView);
-
-
-              //mainBox.getChildren().remove(loading);
           }
           catch (JSONException e)
           {
-             // mainBox.getChildren().remove(loading);
               weather.setPadding(new Insets(0,0,0,30));
               weather.getChildren().add(new Label("Oops...there was a problem, restart app or try again"));
-
               mainBox.getChildren().add(notFound);
               System.out.println("Data not found");
           }
       });
 
       inputData.getChildren().addAll(country,city,currency,button);
-      rates.getChildren().addAll(rate1,rate2);
       top.getChildren().addAll(inputData,rates,weather);
+      mainBox.getChildren().addAll(top);
       primaryStage.setScene(new Scene(mainBox, 1280,720));
       primaryStage.setResizable(false);
       primaryStage.show();
