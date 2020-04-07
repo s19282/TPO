d9b6286957a97d4deb55c14ea3dc97b832c50b20
Mateusz Godlewski <s19282@pjwstk.edu.pl>
2020-04-07 23:20:57 +0200

    kod z sdkp

---
+++ TPO3_GM_S19282/src/zad3/Main.java
@@ -0,0 +1,26 @@
+/**
+ *
+ *  @author Godlewski Mateusz S19282
+ *
+ */
+
+package zad3;
+
+
+public class Main {
+
+  public static void main(String[] args) throws Exception {
+    String fileName = System.getProperty("user.home") + "/PassTimeOptions.yaml";
+    Options opts = Tools.createOptionsFromYaml(fileName);
+    System.out.println(opts);
+    opts.getClientsMap().forEach( (id, dates) -> {
+      System.out.println(id);
+      dates.forEach( dpair -> {
+        String[] d = dpair.split(" +");
+        String info = Time.passed(d[0], d[1]);
+        System.out.println(info);
+      });
+    });
+  }
+
+}
---
+++ TPO3_GM_S19282/src/zad3/Time.java
@@ -0,0 +1,11 @@
+/**
+ *
+ *  @author Godlewski Mateusz S19282
+ *
+ */
+
+package zad3;
+
+
+public class Time {
+}
