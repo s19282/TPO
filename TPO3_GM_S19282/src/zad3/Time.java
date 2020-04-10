ae1c1b6727012d1eaf842249322cab8e7ab4b7b6
Mateusz Godlewski <s19282@pjwstk.edu.pl>
2020-04-11 00:08:07 +0200

    wydzielona metoda additionalLines

--- TPO3_GM_S19282/src/zad3/Time.java
+++ TPO3_GM_S19282/src/zad3/Time.java
@@ -28,7 +28,6 @@ public class Time
                 ZonedDateTime dtFrom = ZonedDateTime.of(LocalDateTime.parse(from),ZoneId.of("Europe/Warsaw"));
                 ZonedDateTime dtTo = ZonedDateTime.of(LocalDateTime.parse(to),ZoneId.of("Europe/Warsaw"));
 
-                //Duration duration = Duration.between(dtFrom,dtTo);
                 long days = ChronoUnit.DAYS.between(dtFrom, dtTo);
                 Period period = Period.between(dtFrom.toLocalDate(),dtTo.toLocalDate());
                 output.append("Od ").append(dtFrom.getDayOfMonth()).append(" ").append(dtFrom.getMonth().name()).append(" ").append(dtFrom.getYear()).append(" (")
@@ -40,46 +39,7 @@ public class Time
                         .append(days%7==0?days/7:String.format("%.2f",days/7D))
                         .append("\n- godzin: ").append(ChronoUnit.HOURS.between(dtFrom, dtTo)).append(", minut: " )
                         .append(ChronoUnit.MINUTES.between(dtFrom, dtTo));
-                if(ChronoUnit.DAYS.between(dtFrom, dtTo)>=1)
-                {
-                    boolean first=true;
-                    output.append("\n- kalendarzowo: ");
-                    if(period.getYears()>=1)
-                    {
-                        output.append(period.getYears());
-                        if(period.getYears()>=5)
-                            output.append(" lat");
-                        else if(period.getYears()>=2)
-                            output.append(" lata");
-                        else
-                            output.append(" rok");
-                        first=false;
-                    }
-                    if(period.getMonths()>=1)
-                    {
-                        if(first)
-                            first=false;
-                        else
-                            output.append(", ");
-                        output.append(period.getMonths());
-                        if(period.getMonths()>=5)
-                            output.append(" miesięcy");
-                        else if(period.getMonths()>=2)
-                            output.append(" miesiące");
-                        else
-                            output.append(" miesiąc");
-                    }
-                    if(period.getDays()>=1)
-                    {
-                        if(!first)
-                            output.append(", ");
-                        output.append(period.getDays());
-                        if(period.getDays()>=2)
-                            output.append(" dni");
-                        else
-                            output.append(" dzień");
-                    }
-                }
+                additionalLines(output, period, ChronoUnit.DAYS.between(dtFrom, dtTo), dtFrom.toLocalDate(), dtTo.toLocalDate());
             }
             catch (DateTimeParseException e)
             {
@@ -101,46 +61,7 @@ public class Time
                         .append(" ").append(dtTo.getYear()).append(" (").append(dtTo.getDayOfWeek())
                         .append(")\n- mija: ").append(days).append(" dni, tygodni ")
                         .append(days%7==0?days/7: String.format("%.2f",days/7D));
-                if(ChronoUnit.DAYS.between(dtFrom, dtTo)>=1)
-                {
-                    boolean first=true;
-                    output.append("\n- kalendarzowo: ");
-                    if(period.getYears()>=1)
-                    {
-                        output.append(period.getYears());
-                        if(period.getYears()>=5)
-                            output.append(" lat");
-                        else if(period.getYears()>=2)
-                            output.append(" lata");
-                        else
-                            output.append(" rok");
-                        first=false;
-                    }
-                    if(period.getMonths()>=1)
-                    {
-                        if(first)
-                            first=false;
-                        else
-                            output.append(", ");
-                        output.append(period.getMonths());
-                        if(period.getMonths()>=5)
-                            output.append(" miesięcy");
-                        else if(period.getMonths()>=2)
-                            output.append(" miesiące");
-                        else
-                            output.append(" miesiąc");
-                    }
-                    if(period.getDays()>=1)
-                    {
-                        if(!first)
-                            output.append(", ");
-                        output.append(period.getDays());
-                        if(period.getDays()>=2)
-                            output.append(" dni");
-                        else
-                            output.append(" dzień");
-                    }
-                }
+                additionalLines(output, period, ChronoUnit.DAYS.between(dtFrom, dtTo), dtFrom, dtTo);
             }
             catch (DateTimeParseException e)
             {
@@ -148,5 +69,48 @@ public class Time
             }
         }
         return output.toString();
+    }
+
+    private static void additionalLines(StringBuilder output, Period period, long between, LocalDate dtFrom, LocalDate dtTo) {
+        if(between >=1)
+        {
+            boolean first=true;
+            output.append("\n- kalendarzowo: ");
+            if(period.getYears()>=1)
+            {
+                output.append(period.getYears());
+                if(period.getYears()>=5)
+                    output.append(" lat");
+                else if(period.getYears()>=2)
+                    output.append(" lata");
+                else
+                    output.append(" rok");
+                first=false;
+            }
+            if(period.getMonths()>=1)
+            {
+                if(first)
+                    first=false;
+                else
+                    output.append(", ");
+                output.append(period.getMonths());
+                if(period.getMonths()>=5)
+                    output.append(" miesięcy");
+                else if(period.getMonths()>=2)
+                    output.append(" miesiące");
+                else
+                    output.append(" miesiąc");
+            }
+            if(period.getDays()>=1)
+            {
+                if(!first)
+                    output.append(", ");
+                output.append(period.getDays());
+                if(period.getDays()>=2)
+                    output.append(" dni");
+                else
+                    output.append(" dzień");
+            }
+        }
     }
 }
