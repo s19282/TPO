430a9f6b60bad29c9fac038da8474e42979032fb
Mateusz Godlewski <s19282@pjwstk.edu.pl>
2020-04-11 14:16:15 +0200

    poprawka dla sdkp

--- TPO3_GM_S19282/src/zad3/Main.java
+++ TPO3_GM_S19282/src/zad3/Main.java
@@ -9,8 +9,7 @@ package zad3;
 
 public class Main {
 
-  public static void main(String[] args)
-  {
+  public static void main(String[] args) throws Exception {
     String fileName = System.getProperty("user.home") + "/PassTimeOptions.yaml";
     Options opts = Tools.createOptionsFromYaml(fileName);
     System.out.println(opts);
--- TPO3_GM_S19282/src/zad3/Tools.java
+++ TPO3_GM_S19282/src/zad3/Tools.java
@@ -10,24 +10,16 @@ package zad3;
 import org.yaml.snakeyaml.Yaml;
 import org.yaml.snakeyaml.constructor.Constructor;
 import java.io.FileInputStream;
-import java.io.IOException;
+import java.io.FileNotFoundException;
 import java.io.InputStream;
 
 
 public class Tools
 {
-    public static Options createOptionsFromYaml(String path)
-    {
-        try(InputStream is = new FileInputStream(path))
-        {
-            Yaml yaml = new Yaml(new Constructor(Options.class));
-            return yaml.load(is);
-        }
-        catch (IOException e)
-        {
-            e.printStackTrace();
-        }
-        return new Options();
+    public static Options createOptionsFromYaml(String path) throws FileNotFoundException {
+        InputStream is = new FileInputStream(path);
+        Yaml yaml = new Yaml(new Constructor(Options.class));
+        return yaml.load(is);
     }
 
 }
